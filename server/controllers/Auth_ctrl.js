8a227bb100d50a3cc98e7f422f331601efe4554f
tortei1000 <tiagomaffei@gmail.com>
2019-06-19 11:13:03 -0600

    auth working on back end

---
+++ server/controllers/Auth_ctrl.js
@@ -0,0 +1,60 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  register : async (req, res)=> {
+    const db = req.app.get('db')
+    const {username, password} = req.body
+
+    let result = await db.get_user(username)
+    let existingUser = result[0]
+
+    if(existingUser){
+      return res.status(409).send(`username taken`)
+    }
+
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+
+    let registeredUser = await db.register_user(username, hash) 
+    let user = registeredUser[0]
+
+    delete user.hash
+    req.session.user = user
+    res.status(201).send(req.session.user)
+  },
+
+  login: async (req, res) => {  
+    const db = req.app.get('db')
+    const {username, password} = req.body
+
+    foundUser = await db.get_user(username)
+    user = foundUser[0]
+    console.log(req.body)
+    
+    if(!user){
+      return res.status(401).send(`username not found`)
+    }
+   
+    isAuthenticated = bcrypt.compareSync(password, user.password) 
+    if(!isAuthenticated){
+      return res.status(401).send(`password doesnt match`)
+    } 
+
+    delete user.hash
+    req.session.user = user
+    res.send(req.session.user)
+      
+  },
+  logout : (req, res) =>{ 
+    req.session.destroy()
+    console.log(`logout fired`)
+    res.sendStatus(200)
+  },
+  getUsers: (req, res) => {
+    console.log(`getUsers was fired`)
+    if(!req.session.user){res.sendStatus(403)}
+    else{
+      res.status(200).send(req.session.user)
+    } 
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const massive = require('massive')
+const session = require('express-session')
+const Auth_ctrl = require('./controllers/Auth_ctrl')
+
+
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
+
+
+
+app.use(express.json())
+
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24
+  }
+}))
+
+massive(CONNECTION_STRING).then((database) => {
+  app.set('db', database)
+  console.log(`1- db is connected`)
+  app.listen(SERVER_PORT, () => {
+    console.log(`2-server is connected on ${SERVER_PORT}`)
+  })
+})
+
+app.post('/auth/login', Auth_ctrl.login)
+app.post('/auth/register', Auth_ctrl.register)
+app.get('/auth/logout', Auth_ctrl.logout)
+app.get('/auth/users', Auth_ctrl.getUsers)
+
+
+
